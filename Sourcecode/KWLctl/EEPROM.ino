5f0506c7a6b427217cc9317367eec6942fef4a0e
Sven Just <sven@familie-just.de>
2018-01-22 13:13:04 +0100

    add GPL Header

--- Sourcecode/KWLctl/EEPROM.ino
+++ Sourcecode/KWLctl/EEPROM.ino
@@ -1,3 +1,29 @@
+################################################################
+#
+#   Copyright notice
+#
+#   Control software for a Room Ventilation System
+#   https://github.com/svenjust/room-ventilation-system
+#    
+#   Copyright (C) 2018  Sven Just (sven@familie-just.de)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#   This copyright notice MUST APPEAR in all copies of the script!
+#
+################################################################
+
 // siehe http://fluuux.de/2013/04/daten-im-eeprom-speichern-auslesen-und-variablen-zuweisen/
 
 /************************************************************
--- Sourcecode/KWLctl/FanCalibration.ino
+++ Sourcecode/KWLctl/FanCalibration.ino
@@ -1,3 +1,30 @@
+################################################################
+#
+#   Copyright notice
+#
+#   Control software for a Room Ventilation System
+#   https://github.com/svenjust/room-ventilation-system
+#    
+#   Copyright (C) 2018  Sven Just (sven@familie-just.de)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#   This copyright notice MUST APPEAR in all copies of the script!
+#
+################################################################
+
+
 /* Die für bestimmte Drehzahl erforderlichen PWM Werte werden ermittelt und abgespeichert.
    Dazu müssen die verschiedenen Drehzahlen für jeden Lüfter per pid-Regler angefahren werden.
 
