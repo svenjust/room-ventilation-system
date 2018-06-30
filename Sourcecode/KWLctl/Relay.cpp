f79bf5e9d38c7ca6473d6946c4c7725932e50db4
Ivan Schreter <schreter@gmx.net>
2018-06-30 20:39:10 +0200

    Work on cleaning up the source code by explicitly specifying user configuration in a separate file.
    Example on IP address, standard settings and relay control.
    
    This method has the advantage of using less space for the configuration
    of complex objects (like IP address), since these are just compile-time
    literals instead of real variables. Thus, more RAM space is available
    for useful stuff.

---
+++ Sourcecode/KWLctl/Relay.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Ivan Schréter (schreter@gmx.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This copyright notice MUST APPEAR in all copies of the software!
+ */
+
+#include "Relay.h"
+#include "kwl_config.h"
+
+void Relay::set_mode(int8_t mode)
+{
+  switch (mode)
+  {
+  case HIGH:
+  case LOW:
+    pinMode(pin_, OUTPUT);
+    digitalWrite(pin_, uint8_t(mode));
+    break;
+
+  default:
+    pinMode(pin_, INPUT);
+  }
+}
+
+void Relay::on()
+{
+  set_mode(kwl_config::RelayON);
+}
+
+void Relay::off()
+{
+  set_mode(kwl_config::RelayOFF);
+}
---
+++ Sourcecode/KWLctl/kwl_config.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2018 Ivan Schréter (schreter@gmx.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This copyright notice MUST APPEAR in all copies of the software!
+ */
+
+#include "kwl_config.h"
+
+#include <IPAddress.h>
+
+IPAddressLiteral::operator IPAddress() const
+{
+  return IPAddress(ip[0], ip[1], ip[2], ip[3]);
+}
+
+constexpr double kwl_default_config::StandardKwlModeFactor[MAX_FAN_MODE_CNT];
