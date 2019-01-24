c5825bd52c333307de7d857288e4bc68c804c557
Sven Just <sven@familie-just.de>
2019-01-24 19:07:38 +0100

    Versionsstring hochgezählt auf V0.22

--- Sourcecode/KWLctl/KWLConfig.h
+++ Sourcecode/KWLctl/KWLConfig.h
@@ -132,7 +132,7 @@ protected:
 public:
 
   // ***************************************************  V E R S I O N S N U M M E R   D E R    S W   *************************************************
-  static constexpr auto VersionString = makeFlashStringLiteral("v0.21");
+  static constexpr auto VersionString = makeFlashStringLiteral("v0.22");
 
 
   // ***************************************************  N E T Z W E R K E I N S T E L L U N G E N *****************************************************
